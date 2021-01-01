1790f6ebfbef70cc004a98e912bda64239d3bad5
mirei-cs <75403367+mirei-cs@users.noreply.github.com>
2021-01-02 00:37:40 +0900

    Update mainja.js

--- mainja.js
+++ mainja.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = '外は華氏94度だったので、 :insertx: は散歩に出かけた。 :inserty: に着くと、 彼らは恐怖のあまりしばらく見つめた後、 :insertz:。ミレイはすべてを見たが、驚きはしなかった。 :insertx: の重さは300ポンドもあり、暑い日だった。';
+let insertX = ['もぐらコロッケ','自分ツッコミくま','ちいかわ','はちわれ'];
+let insertY = ['フエラムネが湧き出てくる場所','朝市場','大きなゼリーが食べられる場所'];
+let insertZ = ['お腹一杯になるまで食べた','口笛を吹いた','インパラに乗った'];
 
 randomize.addEventListener('click', result);
 
